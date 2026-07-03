03b4edbfcf939f1758f7b80c654ce92d98c9a57b
Thisara Chandima <ilangathisara@gmail.com>
2026-07-03 16:06:53 +0530

    feat: Add TURN servers to fix firewall bugs

--- src/pages/Meeting.jsx
+++ src/pages/Meeting.jsx
@@ -94,7 +94,24 @@ function MeetingContent() {
           if (peersRef.current[targetUserId]) return peersRef.current[targetUserId];
           
           const pc = new RTCPeerConnection({
-            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+            iceServers: [
+              { urls: 'stun:stun.l.google.com:19302' },
+              {
+                urls: 'turn:openrelay.metered.ca:80',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+              },
+              {
+                urls: 'turn:openrelay.metered.ca:443',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+              },
+              {
+                urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+              }
+            ]
           });
 
           stream.getTracks().forEach(track => pc.addTrack(track, stream));
